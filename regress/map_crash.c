ebaa542702625d248282f79f4fd4852c3e24e0be
Nguyen Anh Quynh <aquynh@gmail.com>
2015-08-24 20:21:57 +0800

    revert regress/map_crash.c back to original code

--- regress/map_crash.c
+++ regress/map_crash.c
@@ -4,27 +4,27 @@
 #include <stdlib.h>
 
 #define UC_BUG_WRITE_SIZE 13000
-//#define UC_BUG_WRITE_ADDR 0x1000
-#define UC_BUG_WRITE_ADDR 0x2000
+#define UC_BUG_WRITE_ADDR 0x1000    // fix this by change this to 0x2000
 
 int main() {
-	int size;
-	uint8_t *buf;
-	uch uh;
-	uc_err err = uc_open (UC_ARCH_X86, UC_MODE_64, &uh);
-	if (err) {
-		fprintf (stderr, "Cannot initialize unicorn\n");
-		return 1;
-	}
-	size = UC_BUG_WRITE_SIZE;
-	buf = malloc (size);
-	if (!buf) {
-		fprintf (stderr, "Cannot allocate\n");
-		return 1;
-	}
-	memset (buf, 0, size);
-	uc_mem_map (uh, UC_BUG_WRITE_ADDR, size);
-	uc_mem_write (uh, UC_BUG_WRITE_ADDR, buf, size);
-	uc_close (&uh);
-	return 0;
+    int size;
+    uint8_t *buf;
+    uch uh;
+    uc_err err = uc_open (UC_ARCH_X86, UC_MODE_64, &uh);
+    if (err) {
+        fprintf (stderr, "Cannot initialize unicorn\n");
+        return 1;
+    }
+    size = UC_BUG_WRITE_SIZE;
+    buf = malloc (size);
+    if (!buf) {
+        fprintf (stderr, "Cannot allocate\n");
+        return 1;
+    }
+    memset (buf, 0, size);
+    if (!uc_mem_map (uh, UC_BUG_WRITE_ADDR, size)) {
+        uc_mem_write (uh, UC_BUG_WRITE_ADDR, buf, size);
+    }
+    uc_close (&uh);
+    return 0;
 }
