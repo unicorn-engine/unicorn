548355acca90bc8c9151533c478b85208570b28a
Nguyen Anh Quynh <aquynh@gmail.com>
2015-09-11 14:02:27 +0800

    sparc: do not accept BIGENDIAN mode in samples. more sanity check should be done in the core

--- regress/sparc_reg.py
+++ regress/sparc_reg.py
@@ -3,7 +3,7 @@
 from unicorn import *
 from unicorn.sparc_const import *
 
-uc = Uc(UC_ARCH_SPARC, UC_MODE_BIG_ENDIAN)
+uc = Uc(UC_ARCH_SPARC, UC_MODE_32)
 uc.reg_write(UC_SPARC_REG_SP, 100)
 uc.reg_write(UC_SPARC_REG_FP, 100)
 print 'writing sp = 100, fp = 100'
