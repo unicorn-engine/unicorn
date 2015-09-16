f36bd83f854640e595d3ece3e887190fe9fdcc60
Nguyen Anh Quynh <aquynh@gmail.com>
2015-09-16 15:46:10 +0700

    cleanup regress/sparc*.py

--- regress/sparc64.py
+++ regress/sparc64.py
@@ -7,13 +7,13 @@ PAGE_SIZE = 1 * 1024 * 1024
 
 uc = Uc(UC_ARCH_SPARC, UC_MODE_64)
 uc.reg_write(UC_SPARC_REG_SP, 100)
-print 'writing sp = 100, %%i0 = 2000'
+print 'writing sp = 100'
 
-   # 0:	b0 06 20 01 	inc  %i0
-   # 4:	b2 06 60 01 	inc  %i1
+   # 0: b0 06 20 01     inc  %i0
+   # 4: b2 06 60 01     inc  %i1
 
 CODE =  "\xb0\x06\x20\x01" \
-		"\xb2\x06\x60\x01"
+        "\xb2\x06\x60\x01"
 
 uc.mem_map(0, PAGE_SIZE)
 uc.mem_write(0, CODE)
