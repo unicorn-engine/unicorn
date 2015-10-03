fd6b510038232eea02b23d5fdb82b5cf002667e2
Sean Heelan <sean.heelan@gmail.com>
2015-10-03 12:36:17 +0700

    Added a comment explaining what is being tested for

--- tests/regress/ensure_typedef_consts_generated.py
+++ tests/regress/ensure_typedef_consts_generated.py
@@ -1,3 +1,10 @@
+"""See https://github.com/unicorn-engine/unicorn/issues/161
+
+Ensure that constants which are specified via a typedef, rather than an enum,
+are included in the bindings by the script for autogenerating mappings for
+constants.
+"""
+
 import unicorn
 
 try:
