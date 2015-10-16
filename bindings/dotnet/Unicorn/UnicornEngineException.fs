d92bee414dbcc71fca476ba6cb0ec946edd3d19d
Antonio Parata <aparata@gmail.com>
2015-10-16 17:02:49 +0200

    Implemented exception as error management mechanism and removed the error codes

---
+++ bindings/dotnet/Unicorn/UnicornEngineException.fs
@@ -0,0 +1,30 @@
+﻿(*
+
+.NET bindings for the UnicornEngine Emulator Engine
+
+Copyright(c) 2015 Antonio Parata
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*)
+
+namespace UnicornEngine
+
+open System
+
+type UnicornEngineException(errNo: Int32, msg: String) =
+    inherit ApplicationException(msg)
+
+    member this.ErrorNo = errNo
+
