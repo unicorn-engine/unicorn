715d5c3557199d2b9b245f47d082798e8f33ccff
Ryan Hileman <lunixbochs@gmail.com>
2015-10-25 04:32:56 -0700

    Go 1.2 binding compatibility

--- bindings/go/unicorn/hook.go
+++ bindings/go/unicorn/hook.go
@@ -19,43 +19,43 @@ type HookData struct {
 type Hook uint64
 
 //export hookCode
-func hookCode(handle *C.uc_engine, addr uint64, size uint32, user unsafe.Pointer) {
+func hookCode(handle unsafe.Pointer, addr uint64, size uint32, user unsafe.Pointer) {
 	hook := (*HookData)(user)
 	hook.Callback.(func(Unicorn, uint64, uint32))(hook.Uc, uint64(addr), uint32(size))
 }
 
 //export hookMemInvalid
-func hookMemInvalid(handle *C.uc_engine, typ C.uc_mem_type, addr uint64, size int, value int64, user unsafe.Pointer) bool {
+func hookMemInvalid(handle unsafe.Pointer, typ C.uc_mem_type, addr uint64, size int, value int64, user unsafe.Pointer) bool {
 	hook := (*HookData)(user)
 	return hook.Callback.(func(Unicorn, int, uint64, int, int64) bool)(hook.Uc, int(typ), addr, size, value)
 }
 
 //export hookMemAccess
-func hookMemAccess(handle *C.uc_engine, typ C.uc_mem_type, addr uint64, size int, value int64, user unsafe.Pointer) {
+func hookMemAccess(handle unsafe.Pointer, typ C.uc_mem_type, addr uint64, size int, value int64, user unsafe.Pointer) {
 	hook := (*HookData)(user)
 	hook.Callback.(func(Unicorn, int, uint64, int, int64))(hook.Uc, int(typ), addr, size, value)
 }
 
 //export hookInterrupt
-func hookInterrupt(handle *C.uc_engine, intno uint32, user unsafe.Pointer) {
+func hookInterrupt(handle unsafe.Pointer, intno uint32, user unsafe.Pointer) {
 	hook := (*HookData)(user)
 	hook.Callback.(func(Unicorn, uint32))(hook.Uc, intno)
 }
 
 //export hookX86In
-func hookX86In(handle *C.uc_engine, port, size uint32, user unsafe.Pointer) uint32 {
+func hookX86In(handle unsafe.Pointer, port, size uint32, user unsafe.Pointer) uint32 {
 	hook := (*HookData)(user)
 	return hook.Callback.(func(Unicorn, uint32, uint32) uint32)(hook.Uc, port, size)
 }
 
 //export hookX86Out
-func hookX86Out(handle *C.uc_engine, port, size, value uint32, user unsafe.Pointer) {
+func hookX86Out(handle unsafe.Pointer, port, size, value uint32, user unsafe.Pointer) {
 	hook := (*HookData)(user)
 	hook.Callback.(func(Unicorn, uint32, uint32, uint32))(hook.Uc, port, size, value)
 }
 
 //export hookX86Syscall
-func hookX86Syscall(handle *C.uc_engine, user unsafe.Pointer) {
+func hookX86Syscall(handle unsafe.Pointer, user unsafe.Pointer) {
 	hook := (*HookData)(user)
 	hook.Callback.(func(Unicorn))(hook.Uc)
 }
