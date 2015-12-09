1b145f431b610bb76d19813f886f6471fb9ef27a
Nguyen Anh Quynh <aquynh@gmail.com>
2015-12-10 00:53:48 +0800

    code style

--- samples/sample_x86.c
+++ samples/sample_x86.c
@@ -814,14 +814,13 @@ int main(int argc, char **argv, char **envp)
 {
 	// dynamically load shared library
 #ifdef DYNLOAD
-	if( !uc_dyn_load(NULL, 0) )
-	{
-		printf("Error dynamically loading shared library.\n");
-		printf("Please check that unicorn.dll/unicorn.so is available as well as\n");
-		printf("any other dependent dll/so files.\n");
-		printf("The easiest way is to place them in the same directory as this app.\n");
-		return 1;
-	}
+    if (!uc_dyn_load(NULL, 0)) {
+        printf("Error dynamically loading shared library.\n");
+        printf("Please check that unicorn.dll/unicorn.so is available as well as\n");
+        printf("any other dependent dll/so files.\n");
+        printf("The easiest way is to place them in the same directory as this app.\n");
+        return 1;
+    }
 #endif
     
 	if (argc == 2) {
