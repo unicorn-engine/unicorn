288ef1df0aa82868b1c5c022d39131e84016e8db
xorstream <xorstream@users.noreply.github.com>
2016-01-23 12:10:12 +1100

    Fix uc_mode usage in source code

--- include/unicorn/unicorn.h
+++ include/unicorn/unicorn.h
@@ -78,7 +78,7 @@ typedef enum uc_arch {
     UC_ARCH_ARM64,      // ARM-64, also called AArch64
     UC_ARCH_MIPS,       // Mips architecture
     UC_ARCH_X86,        // X86 architecture (including x86 & x86-64)
-    UC_ARCH_PPC,        // PowerPC architecture
+    UC_ARCH_PPC,        // PowerPC architecture (currently unsupported)
     UC_ARCH_SPARC,      // Sparc architecture
     UC_ARCH_M68K,       // M68K architecture
     UC_ARCH_MAX,
