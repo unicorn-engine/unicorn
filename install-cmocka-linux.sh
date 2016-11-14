f9c57cd36475040ec56010058d7a542733e9cc54
Stephen <stephengroat@users.noreply.github.com>
2016-11-14 14:47:27 -0800

    Update install-cmocka-linux.sh

--- install-cmocka-linux.sh
+++ install-cmocka-linux.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
 mkdir cmocka
 wget https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz -O /tmp/cmocka-1.1.0.tar.xz
 tar -xvf /tmp/cmocka-1.1.0.tar.xz -C /tmp
-if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ] ; then
-cd cmocka && which cmake && cmake -G "MinGW Makefiles" /tmp/cmocka-1.1.0 && make
-else
 cd cmocka && cmake /tmp/cmocka-1.1.0 && make
+# cmake builds an so instead of a dll in mingw/msys
+if [[ ! -z $MSYSTEM ]]; then
+cp src/cmocka.so src/cmocka.dll
 fi
-#cmocka does not include headers in build
+# cmocka does not include headers in build
 cp -R /tmp/cmocka-1.1.0/include/ .
