e6d8a477c2e88d3971c0ae9ebf8181c6ee7c5752
Stephen <stephengroat@users.noreply.github.com>
2016-11-15 10:17:15 -0800

    enable cmocka unit testing

--- install-cmocka-linux.sh
+++ install-cmocka-linux.sh
@@ -3,7 +3,7 @@ set -ex
 mkdir cmocka
 wget https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz -O /tmp/cmocka-1.1.0.tar.xz
 tar -xf /tmp/cmocka-1.1.0.tar.xz -C /tmp
-cd cmocka && cmake /tmp/cmocka-1.1.0 && make
+cd cmocka && cmake -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make && make test
 # cmake builds an so instead of a dll in mingw/msys
 if [[ ! -z $MSYSTEM ]]; then
 cp src/cmocka.so src/cmocka.dll
