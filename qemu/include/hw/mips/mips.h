9735c6e28ea82f4d6947443a7ece1313886ed9c0
Nguyen Anh Quynh <aquynh@gmail.com>
2017-01-19 13:46:17 +0800

    cleanup qemu/include/elf.h

--- qemu/include/hw/mips/mips.h
+++ qemu/include/hw/mips/mips.h
@@ -1,14 +1,7 @@
 #ifndef HW_MIPS_H
 #define HW_MIPS_H
-/* Definitions for mips board emulation.  */
-
-/* Kernels can be configured with 64KB pages */
-#define INITRD_PAGE_MASK (~((1 << 16) - 1))
-
-#include "exec/memory.h"
 
 void mips_machine_init(struct uc_struct *uc);
-
 void mips_cpu_register_types(void *opaque);
 
 #endif
