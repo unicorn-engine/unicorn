dcb85e16deb9e2b90c58b87da546cde1e7a268bc
misson20000 <xenotoad@xenotoad.net>
2017-05-11 22:57:19 -0700

    ruby bindings: fix issues with GC freeing callbacks while we still have references to them

--- bindings/ruby/unicorn_gem/ext/unicorn.c
+++ bindings/ruby/unicorn_gem/ext/unicorn.c
@@ -27,14 +27,22 @@ VALUE UnicornModule = Qnil;
 VALUE UcClass = Qnil;
 VALUE UcError = Qnil;
 VALUE SavedContext = Qnil;
+VALUE Hook = Qnil;
 
+struct hook {
+  uc_hook trace;
+  VALUE cb;
+  VALUE ud;
+  VALUE rUc;
+};
 
 void Init_unicorn() {
     rb_require("unicorn/unicorn_const");
     UnicornModule = rb_define_module("Unicorn");
     UcError = rb_define_class_under(UnicornModule, "UcError", rb_eStandardError);
     SavedContext = rb_define_class_under(UnicornModule, "SavedContext", rb_cObject);
-
+    Hook = rb_define_class_under(UnicornModule, "Hook", rb_cObject);
+    
     UcClass = rb_define_class_under(UnicornModule, "Uc", rb_cObject);
     rb_define_method(UcClass, "initialize", m_uc_initialize, 2);
     rb_define_method(UcClass, "emu_start", m_uc_emu_start, -1);
@@ -243,87 +251,87 @@ VALUE m_uc_mem_protect(VALUE self, VALUE address, VALUE size, VALUE perms){
 }
 
 static void cb_hook_code(uc_engine *uc, uint64_t address, uint32_t size, void *user_data){
-    VALUE passthrough = (VALUE)user_data;
+    struct hook *hook = (struct hook *)user_data;
     VALUE cb;
     VALUE ud;
     VALUE rUc;
 
-    cb = rb_ary_entry(passthrough, 0);
-    ud = rb_ary_entry(passthrough, 1);
-    rUc = rb_ary_entry(passthrough, 2);
+    cb = hook->cb;
+    ud = hook->ud;
+    rUc = hook->rUc;
     rb_funcall(cb, rb_intern("call"), 4, rUc, ULL2NUM(address), UINT2NUM(size), ud);
 }
 
 static void cb_hook_mem_access(uc_engine *uc, uint32_t access, uint64_t address, uint32_t size, int64_t value, void *user_data){
-    VALUE passthrough = (VALUE)user_data;
+      struct hook *hook = (struct hook *)user_data;
     VALUE cb;
     VALUE ud;
     VALUE rUc;
 
-    cb = rb_ary_entry(passthrough, 0);
-    ud = rb_ary_entry(passthrough, 1);
-    rUc = rb_ary_entry(passthrough, 2);
-
+    cb = hook->cb;
+    ud = hook->ud;
+    rUc = hook->rUc;
     rb_funcall(cb, rb_intern("call"), 6, rUc, UINT2NUM(access), ULL2NUM(address), UINT2NUM(size), LL2NUM(value), ud);
 }
 
 static bool cb_hook_mem_invalid(uc_engine *uc, uint32_t access, uint64_t address, uint32_t size, int64_t value, void *user_data){
-    VALUE passthrough = (VALUE)user_data;
+    struct hook *hook = (struct hook *)user_data;
     VALUE cb;
     VALUE ud;
     VALUE rUc;
 
-    cb = rb_ary_entry(passthrough, 0);
-    ud = rb_ary_entry(passthrough, 1);
-    rUc = rb_ary_entry(passthrough, 2);
+    cb = hook->cb;
+    ud = hook->ud;
+    rUc = hook->rUc;
+    
     return RTEST(rb_funcall(cb, rb_intern("call"), 6, rUc, UINT2NUM(access), ULL2NUM(address), UINT2NUM(size), LL2NUM(value), ud));
 }
 
 static uint32_t cb_hook_insn_in(uc_engine *uc, uint32_t port, int size, void *user_data){
-    VALUE passthrough = (VALUE)user_data;
+    struct hook *hook = (struct hook *)user_data;
     VALUE cb;
     VALUE ud;
     VALUE rUc;
 
-    cb = rb_ary_entry(passthrough, 0);
-    ud = rb_ary_entry(passthrough, 1);
-    rUc = rb_ary_entry(passthrough, 2);
+    cb = hook->cb;
+    ud = hook->ud;
+    rUc = hook->rUc;
     return NUM2UINT(rb_funcall(cb, rb_intern("call"), 4, rUc, UINT2NUM(port), INT2NUM(size), ud));
 }
 
 static void cb_hook_insn_out(uc_engine *uc, uint32_t port, int size, uint32_t value, void *user_data){
-    VALUE passthrough = (VALUE)user_data;
+    struct hook *hook = (struct hook *)user_data;
     VALUE cb;
     VALUE ud;
     VALUE rUc;
 
-    cb = rb_ary_entry(passthrough, 0);
-    ud = rb_ary_entry(passthrough, 1);
-    rUc = rb_ary_entry(passthrough, 2);
+    cb = hook->cb;
+    ud = hook->ud;
+    rUc = hook->rUc;
     rb_funcall(cb, rb_intern("call"), 5, rUc, UINT2NUM(port), INT2NUM(size), UINT2NUM(value), ud);
 }
 
 static void cb_hook_insn_syscall(uc_engine *uc, void *user_data){
-    VALUE passthrough = (VALUE)user_data;
+    struct hook *hook = (struct hook *)user_data;
     VALUE cb;
     VALUE ud;
     VALUE rUc;
 
-    cb = rb_ary_entry(passthrough, 0);
-    ud = rb_ary_entry(passthrough, 1);
-    rUc = rb_ary_entry(passthrough, 2);
+    cb = hook->cb;
+    ud = hook->ud;
+    rUc = hook->rUc;
     rb_funcall(cb, rb_intern("call"), 2, rUc, ud);
 }
 
 static void cb_hook_intr(uc_engine *uc, uint64_t address, uint32_t size, int64_t value, void *user_data){
-    VALUE passthrough = (VALUE)user_data;
+    struct hook *hook = (struct hook *)user_data;
     VALUE cb;
     VALUE ud;
     VALUE rUc;
 
-    cb = rb_ary_entry(passthrough, 0);
-    ud = rb_ary_entry(passthrough, 1);
-    rUc = rb_ary_entry(passthrough, 2);
+    cb = hook->cb;
+    ud = hook->ud;
+    rUc = hook->rUc;
     rb_funcall(cb, rb_intern("call"), 5, rUc, ULL2NUM(address), UINT2NUM(size), LL2NUM(value), ud);
 }
 
@@ -346,38 +354,43 @@ VALUE m_uc_hook_add(int argc, VALUE* argv, VALUE self){
     if (NIL_P(arg1))
         arg1 = INT2NUM(0);
 
-    VALUE passthrough;
-    uc_hook trace;
     uc_err err;
 
     if (rb_class_of(callback) != rb_cProc)
         rb_raise(UcError, "Expected Proc callback");
 
-    passthrough = rb_ary_new();
-    rb_ary_store(passthrough, 0, callback);
-    rb_ary_store(passthrough, 1, user_data);
-    rb_ary_store(passthrough, 2, self);
+    struct hook *hook = (struct hook *)malloc(sizeof(struct hook));
+    hook->cb = callback;
+    hook->ud = user_data;
+    hook->rUc = self;
 
+    // If I could, I would just mark the Hook object we wind up creating from this,
+    // but noooo, adding VALUEs as GC roots would be too easy. I have to add pointers
+    // to values as GC roots instead.
+    rb_gc_register_address(&hook->cb);
+    rb_gc_register_address(&hook->ud);
+    rb_gc_register_address(&hook->rUc);
+    
     uint32_t htype = NUM2UINT(hook_type);
     if(htype == UC_HOOK_INSN){
             switch(NUM2INT(arg1)){
                 case UC_X86_INS_IN:
-                    err = uc_hook_add(_uc, &trace,  htype, cb_hook_insn_in,(void *)passthrough, NUM2ULL(begin), NUM2ULL(end), NUM2INT(arg1));
+                    err = uc_hook_add(_uc, &hook->trace,  htype, cb_hook_insn_in,(void *)hook, NUM2ULL(begin), NUM2ULL(end), NUM2INT(arg1));
                     break;
                 case UC_X86_INS_OUT:
-                    err = uc_hook_add(_uc, &trace,  htype, cb_hook_insn_out,(void *)passthrough, NUM2ULL(begin), NUM2ULL(end), NUM2INT(arg1));
+                    err = uc_hook_add(_uc, &hook->trace,  htype, cb_hook_insn_out,(void *)hook, NUM2ULL(begin), NUM2ULL(end), NUM2INT(arg1));
                     break;
                 case UC_X86_INS_SYSCALL:
                 case UC_X86_INS_SYSENTER:
-                    err = uc_hook_add(_uc, &trace,  htype, cb_hook_insn_syscall,(void *)passthrough, NUM2ULL(begin), NUM2ULL(end), NUM2INT(arg1));
+                    err = uc_hook_add(_uc, &hook->trace,  htype, cb_hook_insn_syscall,(void *)hook, NUM2ULL(begin), NUM2ULL(end), NUM2INT(arg1));
                     break;
             }
     }
     else if(htype == UC_HOOK_INTR){
-            err = uc_hook_add(_uc, &trace,  htype, cb_hook_intr,(void *)passthrough, NUM2ULL(begin), NUM2ULL(end));
+            err = uc_hook_add(_uc, &hook->trace,  htype, cb_hook_intr,(void *)hook, NUM2ULL(begin), NUM2ULL(end));
     }
     else if(htype == UC_HOOK_CODE || htype == UC_HOOK_BLOCK){
-            err = uc_hook_add(_uc, &trace,  htype, cb_hook_code,(void *)passthrough, NUM2ULL(begin), NUM2ULL(end));
+            err = uc_hook_add(_uc, &hook->trace,  htype, cb_hook_code,(void *)hook, NUM2ULL(begin), NUM2ULL(end));
     }
     else if (htype & UC_HOOK_MEM_READ_UNMAPPED
             || htype & UC_HOOK_MEM_WRITE_UNMAPPED
@@ -391,24 +404,31 @@ VALUE m_uc_hook_add(int argc, VALUE* argv, VALUE self){
             || htype & UC_HOOK_MEM_UNMAPPED
             || htype & UC_HOOK_MEM_PROT
             || htype & UC_HOOK_MEM_INVALID) {
-            err = uc_hook_add(_uc, &trace,  htype, cb_hook_mem_invalid,(void *)passthrough, NUM2ULL(begin), NUM2ULL(end));
+            err = uc_hook_add(_uc, &hook->trace,  htype, cb_hook_mem_invalid,(void *)hook, NUM2ULL(begin), NUM2ULL(end));
     }
     else{
-            err = uc_hook_add(_uc, &trace,  htype, cb_hook_mem_access,(void *)passthrough, NUM2ULL(begin), NUM2ULL(end));
+            err = uc_hook_add(_uc, &hook->trace,  htype, cb_hook_mem_access,(void *)hook, NUM2ULL(begin), NUM2ULL(end));
     }
 
     if (err != UC_ERR_OK) {
       rb_raise(UcError, "%s", uc_strerror(err));
     }
-    return INT2NUM(trace);
+    // ruby isn't allowed to free this because Unicorn keeps a reference to it
+    return Data_Wrap_Struct(Hook, 0, 0, hook);
 }
 
 VALUE m_uc_hook_del(VALUE self, VALUE hook){
-    int h = NUM2INT(hook);
     uc_err err;
     uc_engine *_uc;
     Data_Get_Struct(rb_iv_get(self,"@uch"), uc_engine, _uc);
-    err = uc_hook_del(_uc, h);
+    struct hook *h;
+    Data_Get_Struct(hook, struct hook, h);
+    err = uc_hook_del(_uc, h->trace);
+    rb_gc_register_address(&h->cb);
+    rb_gc_register_address(&h->ud);
+    rb_gc_register_address(&h->rUc);
+    // someone should free that hook struct but not us because the VM still has a reference to it
+
     if (err != UC_ERR_OK) {
       rb_raise(UcError, "%s", uc_strerror(err));
     }
