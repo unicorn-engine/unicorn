e92759e008e0b40dce46d5ecbce86a210ee172be
bulaza <bulaza@users.noreply.github.com>
2017-05-17 11:23:36 -0400

    Update UC_HOOK_MEM_VALID
    UC_HOOK_MEM_READ fires before handlers for invalid memory reads, so UC_HOOK_MEM_VALID would technically also be receiving invalid memory reads. Switching to UC_HOOK_MEM_READ_AFTER ensures that only actually valid reads are hooked

--- include/unicorn/unicorn.h
+++ include/unicorn/unicorn.h
@@ -248,7 +248,7 @@ typedef enum uc_hook_type {
 // Hook type for all events of illegal memory access
 #define UC_HOOK_MEM_INVALID (UC_HOOK_MEM_UNMAPPED + UC_HOOK_MEM_PROT)
 // Hook type for all events of valid memory access
-#define UC_HOOK_MEM_VALID (UC_HOOK_MEM_READ + UC_HOOK_MEM_WRITE + UC_HOOK_MEM_FETCH)
+#define UC_HOOK_MEM_VALID (UC_HOOK_MEM_READ_AFTER + UC_HOOK_MEM_WRITE + UC_HOOK_MEM_FETCH)
 
 /*
   Callback function for hooking memory (READ, WRITE & FETCH)
