7798e55ce8c8278a34307c74355d08ebb733db5a
Stephen <stephengroat@users.noreply.github.com>
2017-06-20 08:46:16 -0700

    move wget to directory option

--- install-cmocka-linux.sh
+++ install-cmocka-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 mkdir -p cmocka
 #wget https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz -O /tmp/cmocka-1.1.0.tar.xz
-wget --no-check-certificate http://cmocka.org/files/1.1/cmocka-1.1.1.tar.xz -O /tmp/
+wget --no-check-certificate https://cmocka.org/files/1.1/cmocka-1.1.1.tar.xz -P /tmp/
 tar -xf /tmp/cmocka-1.1.1.tar.xz -C /tmp
 pushd cmocka && cmake -DUNIT_TESTING=On /tmp/cmocka-1.1.1 && make && make test
 # cmake builds an so instead of a dll in mingw/msys
