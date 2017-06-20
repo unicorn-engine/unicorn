6957349e88e18e684c24406ffed5033526367b5b
Stephen <stephengroat@users.noreply.github.com>
2017-06-20 08:42:24 -0700

    start cmocka install cleanup

--- install-cmocka-linux.sh
+++ install-cmocka-linux.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
-set -ex
+#!/bin/sh -ex
 mkdir -p cmocka
 #wget https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz -O /tmp/cmocka-1.1.0.tar.xz
-wget --no-check-certificate http://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz -O /tmp/cmocka-1.1.0.tar.xz
-tar -xf /tmp/cmocka-1.1.0.tar.xz -C /tmp
-cd cmocka && cmake -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make && make test
+wget --no-check-certificate http://cmocka.org/files/1.1/cmocka-1.1.1.tar.xz -O /tmp/
+tar -xf /tmp/cmocka-1.1.1.tar.xz -C /tmp
+pushd cmocka && cmake -DUNIT_TESTING=On /tmp/cmocka-1.1.1 && make && make test
 # cmake builds an so instead of a dll in mingw/msys
 if [[ ! -z $MSYSTEM ]]; then
 cp src/cmocka.so src/cmocka.dll
