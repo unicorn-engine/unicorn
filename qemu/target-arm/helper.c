e67be36c883e0a6a47b0986a25aaa2ac6d38637a
Nguyen Anh Quynh <aquynh@gmail.com>
2017-12-20 22:12:35 +0800

    arm: remove unused variable in arm_cpu_get_phys_page_debug()

--- qemu/target-arm/helper.c
+++ qemu/target-arm/helper.c
@@ -4335,7 +4335,6 @@ int arm_cpu_handle_mmu_fault(CPUState *cs, vaddr address,
 
 hwaddr arm_cpu_get_phys_page_debug(CPUState *cs, vaddr addr)
 {
-    CPUARMState *env = cs->env_ptr;
     ARMCPU *cpu = ARM_CPU(env->uc, cs);
     hwaddr phys_addr;
     target_ulong page_size;
