20038fb801e2364165a8c6fa936d2747200b27b0
Lioncash <mathew1800@gmail.com>
2018-02-02 08:28:22 -0500

    Backport the PMU feature flag
    Backports the applicable code from commit 929e754d5a621cd53f30e69b766ccf381b58d124 to unicorn

--- qemu/target-arm/cpu64.c
+++ qemu/target-arm/cpu64.c
@@ -85,6 +85,7 @@ static void aarch64_a57_initfn(struct uc_struct *uc, Object *obj, void *opaque)
     set_feature(&cpu->env, ARM_FEATURE_V8_SHA256);
     set_feature(&cpu->env, ARM_FEATURE_V8_PMULL);
     set_feature(&cpu->env, ARM_FEATURE_CRC);
+    set_feature(&cpu->env, ARM_FEATURE_PMU);
     cpu->kvm_target = QEMU_KVM_ARM_TARGET_CORTEX_A57;
     cpu->midr = 0x411fd070;
     cpu->reset_fpsid = 0x41034070;
