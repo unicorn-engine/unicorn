80223e7ad55dc536b11070d42b355e1c3775f471
Aurelien Jarno <aurelien@aurel32.net>
2018-02-10 22:24:03 -0500

    tcg: rename trunc_shr_i32 into trunc_shr_i64_i32
    The op is sometimes named trunc_shr_i32 and sometimes trunc_shr_i64_i32,
    and the name in the README doesn't match the name offered to the
    frontends.
    
    Always use the long name to make it clear it is a size changing op.
    
    Backports commit 0632e555fc4d281d69cb08d98d500d96185b041f from qemu

--- qemu/tcg/tcg-op.c
+++ qemu/tcg/tcg-op.c
@@ -1752,8 +1752,8 @@ void tcg_gen_trunc_shr_i64_i32(TCGContext *s, TCGv_i32 ret, TCGv_i64 arg, unsign
             tcg_gen_mov_i32(s, ret, TCGV_LOW(t));
             tcg_temp_free_i64(s, t);
         }
-    } else if (TCG_TARGET_HAS_trunc_shr_i32) {
-        tcg_gen_op3i_i32(s, INDEX_op_trunc_shr_i32, ret,
+    } else if (TCG_TARGET_HAS_trunc_shr_i64_i32) {
+        tcg_gen_op3i_i32(s, INDEX_op_trunc_shr_i64_i32, ret,
                          MAKE_TCGV_I32(GET_TCGV_I64(arg)), count);
     } else if (count == 0) {
         tcg_gen_mov_i32(s, ret, MAKE_TCGV_I32(GET_TCGV_I64(arg)));
