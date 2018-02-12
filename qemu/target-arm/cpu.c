9087027b8582d34f1638258aeac5eaf57ab98dc9
Fabian Aggeler <aggelerf@ethz.ch>
2018-02-11 19:21:26 -0500

    target-arm: add SCTLR_EL3 and make SCTLR banked
    Implements SCTLR_EL3 and uses secure/non-secure instance when
    needed.
    
    Backports commit 137feaa9a1622620adf19c0b707883dd990738e2 from qemu

--- qemu/target-arm/cpu.c
+++ qemu/target-arm/cpu.c
@@ -105,7 +105,7 @@ static void arm_cpu_reset(CPUState *s)
 #if defined(CONFIG_USER_ONLY)
         env->pstate = PSTATE_MODE_EL0t;
         /* Userspace expects access to DC ZVA, CTL_EL0 and the cache ops */
-        env->cp15.c1_sys |= SCTLR_UCT | SCTLR_UCI | SCTLR_DZE;
+        env->cp15.sctlr_el[1] |= SCTLR_UCT | SCTLR_UCI | SCTLR_DZE;
         /* and to the FP/Neon instructions */
         env->cp15.c1_coproc = deposit64(env->cp15.c1_coproc, 20, 2, 3);
 #else
@@ -173,7 +173,11 @@ static void arm_cpu_reset(CPUState *s)
     // Unicorn: force Thumb mode by setting of uc_open()
     env->thumb = env->uc->thumb;
 
-    if (env->cp15.c1_sys & SCTLR_V) {
+    /* AArch32 has a hard highvec setting of 0xFFFF0000.  If we are currently
+     * executing as AArch32 then check if highvecs are enabled and
+     * adjust the PC accordingly.
+     */
+    if (A32_BANKED_CURRENT_REG_GET(env, sctlr) & SCTLR_V) {
         env->regs[15] = 0xFFFF0000;
     }
 
