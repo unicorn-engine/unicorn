884fe72f543d7a283c7e15c8b822a4e73fced80d
Leon Alrae <leon.alrae@imgtec.com>
2018-02-14 17:01:25 -0500

    target-mips: correct DERET instruction
    Fix Debug Mode flag clearing, and when DERET is placed between LL and SC
    do not make SC fail.
    
    Backports commit fe87c2b36ae9c1c9a5279f3891f3bce1b573baa0 from qemu

--- qemu/target-mips/op_helper.c
+++ qemu/target-mips/op_helper.c
@@ -2162,10 +2162,9 @@ void helper_deret(CPUMIPSState *env)
     debug_pre_eret(env);
     set_pc(env, env->CP0_DEPC);
 
-    env->hflags &= MIPS_HFLAG_DM;
+    env->hflags &= ~MIPS_HFLAG_DM;
     compute_hflags(env);
     debug_post_eret(env);
-    env->lladdr = 1;
 }
 #endif /* !CONFIG_USER_ONLY */
 
