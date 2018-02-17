3b000e4eb3814902030eac81842c5f160939c57c
Yongbok Kim <yongbok.kim@imgtec.com>
2018-02-17 14:31:29 -0500

    target-mips: Add enum for BREAK32
    Add enum for BREAK32
    
    Backports commit dbd8af9824d0ddc4400f859c2af77543461cba0d from qemu

--- qemu/target-mips/translate.c
+++ qemu/target-mips/translate.c
@@ -12117,6 +12117,7 @@ enum {
     MODU = 0x7,
 
     /* The following can be distinguished by their lower 6 bits. */
+    BREAK32 = 0x07,
     INS = 0x0c,
     LSA = 0x0f,
     ALIGN = 0x1f,
@@ -13738,7 +13739,7 @@ static void decode_micromips32_opc(CPUMIPSState *env, DisasContext *ctx)
         case POOL32AXF:
             gen_pool32axf(env, ctx, rt, rs);
             break;
-        case 0x07:
+        case BREAK32:
             generate_exception_end(ctx, EXCP_BREAK);
             break;
         default:
