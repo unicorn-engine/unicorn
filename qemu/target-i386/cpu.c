716da652e75c80af9731907ee16024f87fca8536
Eduardo Habkost <ehabkost@redhat.com>
2018-02-17 15:32:39 -0500

    target-i386: Remove ABM from qemu64 CPU model
    ABM is not available on Sandy Bridge and older, and we want to make the
    default CPU runnable in most hosts, so it won't be enabled by default in
    KVM mode.
    
    We should eventually have all features supported by TCG enabled by
    default in TCG mode, but as we don't have a good mechanism today to
    ensure we have different defaults in KVM and TCG mode, disable ABM in
    the qemu64 CPU model entirely.
    
    Backports commit 711956722c6764336f8b78a2106e57c55f02f36d from qemu

--- qemu/target-i386/cpu.c
+++ qemu/target-i386/cpu.c
@@ -683,8 +683,7 @@ static X86CPUDefinition builtin_x86_defs[] = {
         // FEAT_8000_0001_EDX
             CPUID_EXT2_LM | CPUID_EXT2_SYSCALL | CPUID_EXT2_NX,
         // FEAT_8000_0001_ECX
-            CPUID_EXT3_LAHF_LM | CPUID_EXT3_SVM |
-            CPUID_EXT3_ABM,
+            CPUID_EXT3_LAHF_LM | CPUID_EXT3_SVM,
         },
     },
     {
