86436964b522215ae8433f71f065ef3fa035e1f7
Don Slutz <don.slutz@gmail.com>
2018-02-17 19:11:41 -0500

    exec: Stop using memory after free
    memory_region_unref(mr) can free memory.
    
    For example I got:
    
    Program received signal SIGSEGV, Segmentation fault.
    [Switching to Thread 0x7f43280d4700 (LWP 4462)]
    0x00007f43323283c0 in phys_section_destroy (mr=0x7f43259468b0)
    at /home/don/xen/tools/qemu-xen-dir/exec.c:1023
    1023 if (mr->subpage) {
    (gdb) bt
    at /home/don/xen/tools/qemu-xen-dir/exec.c:1023
    at /home/don/xen/tools/qemu-xen-dir/exec.c:1034
    at /home/don/xen/tools/qemu-xen-dir/exec.c:2205
    (gdb) p mr
    $1 = (MemoryRegion *) 0x7f43259468b0
    
    And this change prevents this.
    
    Backports commit 55b4e80b047300e1512df02887b7448ba3786b62 from qemu

--- qemu/exec.c
+++ qemu/exec.c
@@ -821,9 +821,11 @@ static uint16_t phys_section_add(PhysPageMap *map,
 
 static void phys_section_destroy(MemoryRegion *mr)
 {
+    bool have_sub_page = mr->subpage;
+
     memory_region_unref(mr);
 
-    if (mr->subpage) {
+    if (have_sub_page) {
         subpage_t *subpage = container_of(mr, subpage_t, iomem);
         object_unref(mr->uc, OBJECT(&subpage->iomem));
         g_free(subpage);
