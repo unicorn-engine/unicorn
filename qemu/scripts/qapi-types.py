b190e4887e4a6e9f5bad43c57c48263fb121b1bf
Markus Armbruster <armbru@redhat.com>
2018-02-19 16:11:32 -0500

    qapi: Drop unused and useless parameters and variables
    gen_sync_call()'s parameter indent is useless: gen_sync_call() uses it
    only as optional argument for push_indent() and pop_indent(), their
    default is four, and gen_sync_call()'s only caller passes four. Drop
    the parameter.
    
    gen_visitor_input_containers_decl()'s parameter obj is always
    "QOBJECT(args)". Use that, and drop the parameter.
    
    Drop unused parameters of gen_marshal_output(),
    gen_marshal_input_decl(), generate_visit_struct_body(),
    generate_visit_list(), generate_visit_enum(), generate_declaration(),
    generate_enum_declaration(), generate_decl_enum().
    
    Drop unused variables in generate_event_enum_lookup(),
    generate_enum_lookup(), generate_visit_struct_fields(), check_event().
    
    Backports commit 5aa05d3f72e556752167f7005d6a3dea0f4432c5 from qemu

--- qemu/scripts/qapi-types.py
+++ qemu/scripts/qapi-types.py
@@ -108,7 +108,6 @@ def generate_enum_lookup(name, values):
 const char * const %(name)s_lookup[] = {
 ''',
                 name=c_name(name))
-    i = 0
     for value in values:
         ret += mcgen('''
     "%(value)s",
