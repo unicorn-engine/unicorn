00cf2aaef2131006b9bcd1a61ad9e68c701f9e35
Peter Maydell <peter.maydell@linaro.org>
2018-02-19 00:44:58 -0500

    mips: Clean up includes
    Clean up includes so that osdep.h is included first and headers
    which it implies are not included manually.
    
    This commit was created with scripts/clean-includes.
    
    Backports commit c684822ad29968af09735148f03a511bc514589d from qemu

--- qemu/target-mips/helper.c
+++ qemu/target-mips/helper.c
@@ -16,12 +16,9 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+
+#include "qemu/osdep.h"
 #include "unicorn/platform.h"
-#include <signal.h>
 
 #include "cpu.h"
 #include "exec/cpu_ldst.h"
--- qemu/target-mips/translate.c
+++ qemu/target-mips/translate.c
@@ -21,6 +21,7 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "qemu/osdep.h"
 #include "cpu.h"
 #include "tcg-op.h"
 #include "exec/cpu_ldst.h"
