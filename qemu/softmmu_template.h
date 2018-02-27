5c79851143a7002bd896936bb3abe35e48513d0c
Richard Henderson <rth@twiddle.net>
2018-02-27 12:36:21 -0500

    cputlb: Tidy some macros
    TGT_LE and TGT_BE are not size dependent and do not need to be
    redefined. The others are no longer used at all.
    
    Backports commit c86c6e4c80fee4d9423bedb10ba9e9c4aa68f861 from qemu

--- qemu/softmmu_template.h
+++ qemu/softmmu_template.h
@@ -85,14 +85,6 @@
 # define BSWAP(X)  (X)
 #endif
 
-#ifdef TARGET_WORDS_BIGENDIAN
-# define TGT_BE(X)  (X)
-# define TGT_LE(X)  BSWAP(X)
-#else
-# define TGT_BE(X)  BSWAP(X)
-# define TGT_LE(X)  (X)
-#endif
-
 #if DATA_SIZE == 1
 # define helper_le_ld_name  glue(glue(helper_ret_ld, USUFFIX), MMUSUFFIX)
 # define helper_be_ld_name  helper_le_ld_name
@@ -107,14 +99,6 @@
 # define helper_be_lds_name glue(glue(helper_be_ld, SSUFFIX), MMUSUFFIX)
 # define helper_le_st_name  glue(glue(helper_le_st, SUFFIX), MMUSUFFIX)
 # define helper_be_st_name  glue(glue(helper_be_st, SUFFIX), MMUSUFFIX)
-#endif
-
-#ifdef TARGET_WORDS_BIGENDIAN
-# define helper_te_ld_name  helper_be_ld_name
-# define helper_te_st_name  helper_be_st_name
-#else
-# define helper_te_ld_name  helper_le_ld_name
-# define helper_te_st_name  helper_le_st_name
 #endif
 
 #ifndef SOFTMMU_CODE_ACCESS
@@ -819,15 +803,9 @@ void helper_be_st_name(CPUArchState *env, target_ulong addr, DATA_TYPE val,
 #undef USUFFIX
 #undef SSUFFIX
 #undef BSWAP
-#undef TGT_BE
-#undef TGT_LE
-#undef CPU_BE
-#undef CPU_LE
 #undef helper_le_ld_name
 #undef helper_be_ld_name
 #undef helper_le_lds_name
 #undef helper_be_lds_name
 #undef helper_le_st_name
 #undef helper_be_st_name
-#undef helper_te_ld_name
-#undef helper_te_st_name
