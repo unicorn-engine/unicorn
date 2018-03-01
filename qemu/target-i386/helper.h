22ebc5fceea25e894d317c1654b38e3c70353ef1
Richard Henderson <rth@twiddle.net>
2018-03-01 16:14:50 -0500

    target-i386: Use clz and ctz opcodes
    Backports commit e5143c90883cd32a432eb793cdcce6bee747834a from qemu

--- qemu/target-i386/helper.h
+++ qemu/target-i386/helper.h
@@ -203,14 +203,6 @@ DEF_HELPER_FLAGS_3(xsetbv, TCG_CALL_NO_WG, void, env, i32, i64)
 DEF_HELPER_FLAGS_2(rdpkru, TCG_CALL_NO_WG, i64, env, i32)
 DEF_HELPER_FLAGS_3(wrpkru, TCG_CALL_NO_WG, void, env, i32, i64)
 
-DEF_HELPER_FLAGS_1(clz_x86, TCG_CALL_NO_RWG_SE, tl, tl)
-
-#ifdef TARGET_I386
-#define helper_clz helper_clz_x86
-#define gen_helper_clz gen_helper_clz_x86
-#endif
-
-DEF_HELPER_FLAGS_1(ctz, TCG_CALL_NO_RWG_SE, tl, tl)
 DEF_HELPER_FLAGS_2(pdep, TCG_CALL_NO_RWG_SE, tl, tl, tl)
 DEF_HELPER_FLAGS_2(pext, TCG_CALL_NO_RWG_SE, tl, tl, tl)
 
