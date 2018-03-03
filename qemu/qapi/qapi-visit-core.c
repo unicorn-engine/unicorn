a57d8a5b504abcfd494fb695a7c46aa578232b43
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-03-03 18:34:30 -0500

    qapi: Remove visit_start_alternate() parameter promote_int
    Before the previous commit, parameter promote_int = true made
    visit_start_alternate() with an input visitor avoid QTYPE_QINT
    variants and create QTYPE_QFLOAT variants instead. This was used
    where QTYPE_QINT variants were invalid.
    
    The previous commit fused QTYPE_QINT with QTYPE_QFLOAT, rendering
    promote_int useless and unused.
    
    Backports commit 60390d2dc85ffade8981ca41e02335cb07353a6d from qemu

--- qemu/qapi/qapi-visit-core.c
+++ qemu/qapi/qapi-visit-core.c
@@ -99,12 +99,12 @@ void visit_end_list(Visitor *v, void **obj)
 
 void visit_start_alternate(Visitor *v, const char *name,
                            GenericAlternate **obj, size_t size,
-                           bool promote_int, Error **errp)
+                           Error **errp)
 {
     assert(obj && size >= sizeof(GenericAlternate));
     assert(!(v->type & VISITOR_OUTPUT) || *obj);
     if (v->start_alternate) {
-        v->start_alternate(v, name, obj, size, promote_int, errp);
+        v->start_alternate(v, name, obj, size, errp);
     }
 }
 
