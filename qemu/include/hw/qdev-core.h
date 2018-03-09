af3cd62c4b419bdf856441241fde4c61afbe8fcc
Thomas Huth <thuth@redhat.com>
2018-03-09 13:00:22 -0500

    Introduce DEVICE_CATEGORY_CPU for CPU devices
    Now that CPUs show up in the help text of "-device ?",
    we should group them into an appropriate category.
    
    Backports commit ba31cc7226ebcee639f18faa90c1542bd364fba3 from qemu

--- qemu/include/hw/qdev-core.h
+++ qemu/include/hw/qdev-core.h
@@ -25,6 +25,7 @@ typedef enum DeviceCategory {
     DEVICE_CATEGORY_DISPLAY,
     DEVICE_CATEGORY_SOUND,
     DEVICE_CATEGORY_MISC,
+    DEVICE_CATEGORY_CPU,
     DEVICE_CATEGORY_MAX
 } DeviceCategory;
 
