81ae246f076d34bc5c41bb3dd8bb0f4ef7768424
Richard Henderson <richard.henderson@linaro.org>
2018-03-09 01:12:18 -0500

    target/arm: Enable ARM_FEATURE_V8_FCMA
    Enable it for the "any" CPU used by *-linux-user.
    
    Backports commit e66a67bf28e1b4fce2e3d72a2610dbd48d9d3078 from qemu

--- qemu/target/arm/cpu.c
+++ qemu/target/arm/cpu.c
@@ -1475,6 +1475,7 @@ static void arm_any_initfn(struct uc_struct *uc, Object *obj, void *opaque)
     set_feature(&cpu->env, ARM_FEATURE_V8_PMULL);
     set_feature(&cpu->env, ARM_FEATURE_CRC);
     set_feature(&cpu->env, ARM_FEATURE_V8_RDM);
+    set_feature(&cpu->env, ARM_FEATURE_V8_FCMA);
     cpu->midr = 0xffffffff;
 }
 #endif
