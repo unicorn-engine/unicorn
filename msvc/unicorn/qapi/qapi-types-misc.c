311d659acfd9c022c34358577283fb79e50a8466
Markus Armbruster <armbru@redhat.com>
2018-03-09 10:13:16 -0500

    qapi: Empty out qapi-schema.json
    The previous commit improved compile time by including less of the
    generated QAPI headers. This is impossible for stuff defined directly
    in qapi-schema.json, because that ends up in headers that that pull in
    everything.
    
    Move everything but include directives from qapi-schema.json to new
    sub-module qapi/misc.json, then include just the "misc" shard where
    possible.
    
    It's possible everywhere, except:
    
    * monitor.c needs qmp-command.h to get qmp_init_marshal()
    
    * monitor.c, ui/vnc.c and the generated qapi-event-FOO.c need
    qapi-event.h to get enum QAPIEvent
    
    Perhaps we'll get rid of those some other day.
    
    Adding a type to qapi/migration.json now recompiles some 120 instead
    of 2300 out of 5100 objects.
    
    Backports commit 112ed241f5d9a411dbca92bdf597151cb853c6a7 from qemu

---
+++ msvc/unicorn/qapi/qapi-types-misc.c
@@ -0,0 +1,69 @@
+/* AUTOMATICALLY GENERATED, DO NOT MODIFY */
+
+/*
+ * Schema-defined QAPI types
+ *
+ * Copyright IBM, Corp. 2011
+ * Copyright (c) 2013-2018 Red Hat Inc.
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2.1 or later.
+ * See the COPYING.LIB file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/dealloc-visitor.h"
+#include "qapi/qapi-types-misc.h"
+#include "qapi/qapi-visit-misc.h"
+
+const char *const X86CPURegister32_lookup[] = {
+    "EAX",
+    "EBX",
+    "ECX",
+    "EDX",
+    "ESP",
+    "EBP",
+    "ESI",
+    "EDI",
+    NULL,
+};
+
+void qapi_free_X86CPUFeatureWordInfo(X86CPUFeatureWordInfo *obj)
+{
+    Visitor *v;
+
+    if (!obj) {
+        return;
+    }
+
+    v = qapi_dealloc_visitor_new();
+    visit_type_X86CPUFeatureWordInfo(v, NULL, &obj, NULL);
+    visit_free(v);
+}
+
+void qapi_free_X86CPUFeatureWordInfoList(X86CPUFeatureWordInfoList *obj)
+{
+    Visitor *v;
+
+    if (!obj) {
+        return;
+    }
+
+    v = qapi_dealloc_visitor_new();
+    visit_type_X86CPUFeatureWordInfoList(v, NULL, &obj, NULL);
+    visit_free(v);
+}
+
+void qapi_free_DummyForceArrays(DummyForceArrays *obj)
+{
+    Visitor *v;
+
+    if (!obj) {
+        return;
+    }
+
+    v = qapi_dealloc_visitor_new();
+    visit_type_DummyForceArrays(v, NULL, &obj, NULL);
+    visit_free(v);
+}
+/* Dummy declaration to prevent empty .o file */
+char dummy_qapi_qapi_types_misc_c;
---
+++ msvc/unicorn/qapi/qapi-types-misc.h
@@ -0,0 +1,61 @@
+/* AUTOMATICALLY GENERATED, DO NOT MODIFY */
+
+/*
+ * Schema-defined QAPI types
+ *
+ * Copyright IBM, Corp. 2011
+ * Copyright (c) 2013-2018 Red Hat Inc.
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2.1 or later.
+ * See the COPYING.LIB file in the top-level directory.
+ */
+
+#ifndef QAPI_QAPI_TYPES_MISC_H
+#define QAPI_QAPI_TYPES_MISC_H
+
+#include "qapi-builtin-types.h"
+
+typedef enum X86CPURegister32 {
+    X86_CPU_REGISTER32_EAX = 0,
+    X86_CPU_REGISTER32_EBX = 1,
+    X86_CPU_REGISTER32_ECX = 2,
+    X86_CPU_REGISTER32_EDX = 3,
+    X86_CPU_REGISTER32_ESP = 4,
+    X86_CPU_REGISTER32_EBP = 5,
+    X86_CPU_REGISTER32_ESI = 6,
+    X86_CPU_REGISTER32_EDI = 7,
+    X86_CPU_REGISTER32__MAX = 8,
+} X86CPURegister32;
+
+extern const char *const X86CPURegister32_lookup[];
+
+typedef struct X86CPUFeatureWordInfo X86CPUFeatureWordInfo;
+
+typedef struct X86CPUFeatureWordInfoList X86CPUFeatureWordInfoList;
+
+typedef struct DummyForceArrays DummyForceArrays;
+
+struct X86CPUFeatureWordInfo {
+    int64_t cpuid_input_eax;
+    bool has_cpuid_input_ecx;
+    int64_t cpuid_input_ecx;
+    X86CPURegister32 cpuid_register;
+    int64_t features;
+};
+
+void qapi_free_X86CPUFeatureWordInfo(X86CPUFeatureWordInfo *obj);
+
+struct X86CPUFeatureWordInfoList {
+    X86CPUFeatureWordInfoList *next;
+    X86CPUFeatureWordInfo *value;
+};
+
+void qapi_free_X86CPUFeatureWordInfoList(X86CPUFeatureWordInfoList *obj);
+
+struct DummyForceArrays {
+    X86CPUFeatureWordInfoList *unused;
+};
+
+void qapi_free_DummyForceArrays(DummyForceArrays *obj);
+
+#endif /* QAPI_QAPI_TYPES_MISC_H */
