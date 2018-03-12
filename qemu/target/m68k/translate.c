5d508f45b6a2d90e6a859d2d7fd3590ec9340506
Laurent Vivier <laurent@vivier.eu>
2018-03-12 09:32:37 -0400

    target/m68k: implement fetox
    Using a local m68k floatx80_etox()
    [copied from previous:
    Written by Andreas Grabher for Previous, NeXT Computer Emulator.]
    
    Backports commit 40ad087330bee5394c9e78c97f909f580be69b58 from qemu

--- qemu/target/m68k/translate.c
+++ qemu/target/m68k/translate.c
@@ -5256,6 +5256,9 @@ DISAS_INSN(fpu)
     case 0x06: /* flognp1 */
         gen_helper_flognp1(tcg_ctx, tcg_ctx->cpu_env, cpu_dest, cpu_src);
         break;
+    case 0x10: /* fetox */
+        gen_helper_fetox(tcg_ctx, tcg_ctx->cpu_env, cpu_dest, cpu_src);
+        break;
     case 0x14: /* flogn */
         gen_helper_flogn(tcg_ctx, tcg_ctx->cpu_env, cpu_dest, cpu_src);
         break;
