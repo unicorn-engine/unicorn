14653b00dbc79ef32b44355a64c1a77e01189d4c
Lioncash <mathew1800@gmail.com>
2018-03-20 11:25:30 -0400

    qobject: let object_property_get_str() use new API

--- qemu/qom/object.c
+++ qemu/qom/object.c
@@ -1008,18 +1008,15 @@ char *object_property_get_str(struct uc_struct *uc, Object *obj, const char *nam
                               Error **errp)
 {
     QObject *ret = object_property_get_qobject(uc, obj, name, errp);
-    QString *qstring;
     char *retval;
 
     if (!ret) {
         return NULL;
     }
-    qstring = qobject_to(QString, ret);
-    if (!qstring) {
+
+    retval = g_strdup(qobject_get_try_str(ret));
+    if (!retval) {
         error_setg(errp, QERR_INVALID_PARAMETER_TYPE, name, "string");
-        retval = NULL;
-    } else {
-        retval = g_strdup(qstring_get_str(qstring));
     }
 
     qobject_decref(ret);
