1a9d9e35475755b248d311b3cfb45867a302258e
Nicholas Piggin <npiggin@gmail.com>
2018-06-15 11:53:45 -0400

    osdep: powerpc64 align memory to allow 2MB radix THP page tables
    This allows KVM with the Book3S radix MMU mode to take advantage of
    THP and install larger pages in the partition scope page tables (the
    host translation).
    
    Backports commit 0c1272cc7c72dfe0ef66be8f283cf67c74b58586 from qemu

--- qemu/util/oslib-posix.c
+++ qemu/util/oslib-posix.c
@@ -27,7 +27,8 @@
  */
 
 #if defined(__linux__) && \
-    (defined(__x86_64__) || defined(__arm__) || defined(__aarch64__))
+    (defined(__x86_64__) || defined(__arm__) || defined(__aarch64__) \
+     || defined(__powerpc64__))
    /* Use 2 MiB alignment so transparent hugepages can be used by KVM.
       Valgrind does not support alignments larger than 1 MiB,
       therefore we need special code which handles running on Valgrind. */
