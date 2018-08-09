0f14c473445661a633ca5bda3a91ba1a87c35c64
toshiMSFT <42048361+toshiMSFT@users.noreply.github.com>
2018-08-09 08:32:31 -0700

    Makes SYSENTER hookable again on x86 (#996)
    Adds SYSENTER to the whitelist of supported hookable instructions in unicorn
    as well as fixes up the existing sysenter_hook_x86 regression test which was
    previously failing
    
    Fixes unicorn-engine/unicorn#995

--- qemu/target-i386/unicorn.c
+++ qemu/target-i386/unicorn.c
@@ -1341,7 +1341,8 @@ static bool x86_insn_hook_validate(uint32_t insn_enum)
     //for x86 we can only hook IN, OUT, and SYSCALL
     if (insn_enum != UC_X86_INS_IN
         &&  insn_enum != UC_X86_INS_OUT
-        &&  insn_enum != UC_X86_INS_SYSCALL) {
+        &&  insn_enum != UC_X86_INS_SYSCALL
+        &&  insn_enum != UC_X86_INS_SYSENTER) {
         return false;
     }
     return true;
