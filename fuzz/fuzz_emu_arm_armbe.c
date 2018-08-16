070fdad8c51bbbf011808a364347a18afef650c4
Philippe Antoine <contact@catenacyber.fr>
2018-08-16 19:48:33 +0200

    Redirect fuzz output somewhere else than stdout

--- fuzz/fuzz_emu_arm_armbe.c
+++ fuzz/fuzz_emu_arm_armbe.c
@@ -6,6 +6,8 @@
 
 uc_engine *uc;
 int initialized = 0;
+FILE * outfile = NULL;
+
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     uc_err err;
@@ -21,6 +23,16 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
         // map 4MB memory for this emulation
         uc_mem_map(uc, ADDRESS, 4 * 1024 * 1024, UC_PROT_ALL);
 
+        if (outfile == NULL) {
+            // we compute the output
+            outfile = fopen("/dev/null", "w");
+            if (outfile == NULL) {
+                printf("failed opening /dev/null\n");
+                abort();
+                return 0;
+            }
+        }
+
         initialized = 1;
     }
 
@@ -33,8 +45,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     // emulate code in infinite time & unlimited instructions
     err=uc_emu_start(uc, ADDRESS, ADDRESS + Size, 0, 0);
     if (err) {
-        printf("Failed on uc_emu_start() with error returned %u: %s\n",
-               err, uc_strerror(err));
+        fprintf(outfile, "Failed on uc_emu_start() with error returned %u: %s\n", err, uc_strerror(err));
     }
 
     //no closing for global variable uc_close(uc);
