6d47b38b7f8b6de0ee96a93a91180fafe2f01525
Nguyen Anh Quynh <aquynh@gmail.com>
2019-02-28 09:56:29 +0800

    bindings: update after recent addition of ARM_REG_IPSR

--- bindings/dotnet/UnicornManaged/Const/Arm.fs
+++ bindings/dotnet/UnicornManaged/Const/Arm.fs
@@ -123,7 +123,8 @@ module Arm =
     let UC_ARM_REG_C1_C0_2 = 111
     let UC_ARM_REG_C13_C0_2 = 112
     let UC_ARM_REG_C13_C0_3 = 113
-    let UC_ARM_REG_ENDING = 114
+    let UC_ARM_REG_IPSR = 114
+    let UC_ARM_REG_ENDING = 115
 
     // alias registers
     let UC_ARM_REG_R13 = 12
--- bindings/java/unicorn/ArmConst.java
+++ bindings/java/unicorn/ArmConst.java
@@ -120,7 +120,8 @@ public interface ArmConst {
    public static final int UC_ARM_REG_C1_C0_2 = 111;
    public static final int UC_ARM_REG_C13_C0_2 = 112;
    public static final int UC_ARM_REG_C13_C0_3 = 113;
-   public static final int UC_ARM_REG_ENDING = 114;
+   public static final int UC_ARM_REG_IPSR = 114;
+   public static final int UC_ARM_REG_ENDING = 115;
 
 // alias registers
    public static final int UC_ARM_REG_R13 = 12;
