581e49b9ba420dfd6fd42c466d04464a80d481d1
chenhuitao <h980501427@163.com>
2020-01-07 16:44:56 +0800

    fix oss-fuzz 11112.

--- qemu/target-i386/ops_sse.h
+++ qemu/target-i386/ops_sse.h
@@ -1469,8 +1469,8 @@ void glue(helper_phsubw, SUFFIX)(CPUX86State *env, Reg *d, Reg *s)
 
 void glue(helper_phsubd, SUFFIX)(CPUX86State *env, Reg *d, Reg *s)
 {
-    d->L(0) = (int32_t)d->L(0) - (int32_t)d->L(1);
-    XMM_ONLY(d->L(1) = (int32_t)d->L(2) - (int32_t)d->L(3));
+    d->L(0) = (int32_t)((int64_t)d->L(0) - (int64_t)d->L(1));
+    XMM_ONLY(d->L(1) = (int32_t)((int64_t)d->L(2) - (int64_t)d->L(3)));
     d->L((1 << SHIFT) + 0) = (uint32_t)((int32_t)s->L(0) - (int32_t)s->L(1));
     XMM_ONLY(d->L(3) = (int32_t)s->L(2) - (int32_t)s->L(3));
 }
