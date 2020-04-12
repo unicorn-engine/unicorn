1cf23bb05d5b7cabdf10a2a60db5c6add40fac1c
chenhuitao <h980501427@163.com>
2020-04-13 01:16:08 +0800

    fix oss-fuzz 21651.

--- qemu/target-arm/neon_helper.c
+++ qemu/target-arm/neon_helper.c
@@ -698,7 +698,7 @@ NEON_VOP(rshl_s16, neon_s16, 2)
  * intermediate 64 bit accumulator.  */
 uint32_t HELPER(neon_rshl_s32)(uint32_t valop, uint32_t shiftop)
 {
-    int32_t dest;
+    uint32_t dest;
     int32_t val = (int32_t)valop;
     int8_t shift = (int8_t)shiftop;
     if ((shift >= 32) || (shift <= -32)) {
@@ -707,7 +707,7 @@ uint32_t HELPER(neon_rshl_s32)(uint32_t valop, uint32_t shiftop)
         int64_t big_dest = ((int64_t)val + (1ULL << (-1 - shift)));
         dest = big_dest >> -shift;
     } else {
-        dest = val << shift;
+        dest = (uint32_t)val << shift;
     }
     return dest;
 }
