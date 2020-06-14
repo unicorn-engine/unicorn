961fed7b11f9edb541abc4bdc9d65ee76f7da18e
Nguyen Anh Quynh <aquynh@gmail.com>
2020-06-15 00:51:28 +0800

    clean cpus.h

--- qemu/include/sysemu/cpus.h
+++ qemu/include/sysemu/cpus.h
@@ -7,14 +7,8 @@ struct uc_struct;
 int resume_all_vcpus(struct uc_struct*);
 void cpu_stop_current(struct uc_struct*);
 
-#ifndef CONFIG_USER_ONLY
 /* vl.c */
 extern int smp_cores;
 extern int smp_threads;
-#else
-/* *-user doesn't have configurable SMP topology */
-#define smp_cores   1
-#define smp_threads 1
-#endif
 
 #endif
