ff10e00c2be2dfdc2a32a74254394535599f552e
Nicolae Mogoreanu <mogo@google.com>
2020-09-15 10:51:05 -0700

    Removed unused bitmap_set rename bitmap_clear to qemu_bitmap_clear.
    When unicorn and systemd are included into a single binary they conflict
    on the bitmap_clear function which breaks unicorn. It seems that
    long-term we can remove the qemu_bitmap_clear altogether and leave only
    bitmap_zero_extend.

--- qemu/util/bitmap.c
+++ qemu/util/bitmap.c
@@ -14,27 +14,7 @@
 
 #define BITMAP_FIRST_WORD_MASK(start) (~0UL << ((start) % BITS_PER_LONG))
 
-void bitmap_set(unsigned long *map, long start, long nr)
-{
-    unsigned long *p = map + BIT_WORD(start);
-    const long size = start + nr;
-    int bits_to_set = BITS_PER_LONG - (start % BITS_PER_LONG);
-    unsigned long mask_to_set = BITMAP_FIRST_WORD_MASK(start);
-
-    while (nr - bits_to_set >= 0) {
-        *p |= mask_to_set;
-        nr -= bits_to_set;
-        bits_to_set = BITS_PER_LONG;
-        mask_to_set = ~0UL;
-        p++;
-    }
-    if (nr) {
-        mask_to_set &= BITMAP_LAST_WORD_MASK(size);
-        *p |= mask_to_set;
-    }
-}
-
-void bitmap_clear(unsigned long *map, long start, long nr)
+void qemu_bitmap_clear(unsigned long *map, long start, long nr)
 {
     unsigned long *p = map + BIT_WORD(start);
     const long size = start + nr;
