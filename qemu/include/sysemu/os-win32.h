b61ea6a8b404c0d9529707937e55e7025434a981
Mio <a771644612@live.com>
2020-09-21 20:22:24 +0800

    Fix signature

--- qemu/include/sysemu/os-win32.h
+++ qemu/include/sysemu/os-win32.h
@@ -62,7 +62,7 @@
  * That is what we need for QEMU. Passing the value of register rsp (default)
  * lets longjmp try a stack unwinding which will crash with generated code. */
 
-void _setjmp_wrapper(jmp_buf);
+int _setjmp_wrapper(jmp_buf);
 
 # undef setjmp
 # define setjmp(env) _setjmp_wrapper(env)
