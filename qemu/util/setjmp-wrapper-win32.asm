75840ad29cc62f10623386c88b9537758eaf1b17
Mio <a771644612@live.com>
2020-09-21 22:40:36 +0800

    Add comments

--- qemu/util/setjmp-wrapper-win32.asm
+++ qemu/util/setjmp-wrapper-win32.asm
@@ -9,6 +9,13 @@ _setjmp_wrapper PROC
 ; Short answer: Windows default implementation of setjmp/longjmp is incompatible with generated code.
 ; A longer answer: https://blog.lazym.io/2020/09/21/Unicorn-Devblog-setjmp-longjmp-on-Windows/.
 
+; From qemu os-win32 comments:
+; > On w64, setjmp is implemented by _setjmp which needs a second parameter.
+; > If this parameter is NULL, longjump does no stack unwinding.
+; > That is what we need for QEMU. Passing the value of register rsp (default)
+; > lets longjmp try a stack unwinding which will crash with generated code.
+; It's true indeed, but MSVC doesn't has a setjmp signature which receives two arguements.
+; Therefore, we add a wrapper to keep the second argument zero.
 xor rdx, rdx
 jmp _setjmp
 
