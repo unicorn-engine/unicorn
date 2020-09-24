a1d105fb2d467a4889b7f5ab1b006d45716764a6
Mio <a771644612@live.com>
2020-09-24 17:42:32 +0800

    Use uc_context_free for python bindings

--- bindings/python/unicorn/unicorn.py
+++ bindings/python/unicorn/unicorn.py
@@ -139,6 +139,7 @@ _setup_prototype(_uc, "uc_free", ucerr, ctypes.c_void_p)
 _setup_prototype(_uc, "uc_context_save", ucerr, uc_engine, uc_context)
 _setup_prototype(_uc, "uc_context_restore", ucerr, uc_engine, uc_context)
 _setup_prototype(_uc, "uc_context_size", ctypes.c_size_t, uc_engine)
+_setup_prototype(_uc, "uc_context_free", ucerr, uc_context)
 _setup_prototype(_uc, "uc_mem_regions", ucerr, uc_engine, ctypes.POINTER(ctypes.POINTER(_uc_mem_region)), ctypes.POINTER(ctypes.c_uint32))
 
 # uc_hook_add is special due to variable number of arguments
@@ -661,7 +662,7 @@ class UcContext:
     def __del__(self):
         # We need this property since we shouldn't free it if the object is constructed from pickled bytes.
         if self._to_free:
-            _uc.uc_free(self._context)
+            _uc.uc_context_free(self._context)
 
 
 # print out debugging info
