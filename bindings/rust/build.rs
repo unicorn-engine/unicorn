7c23950094efa18d14c81adb27f10246a2794196
moonshadow565 <moonshadow565@hotmail.com>
2021-10-13 22:52:06 +0200

    only use ninja on msvc and only if other generator is not present

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,10 +1,12 @@
-use cmake;
-
-// To get ninja fetch from here and drop .exe in your PATH:
-// https://github.com/ninja-build/ninja/releases
 fn main() {
     let mut config = cmake::Config::new("../../.");
-    let target = config.generator("Ninja").build_target("unicorn");
+
+    #[cfg(target_env = "msvc")]
+    if !std::env::var("CMAKE_GENERATOR").is_ok() {
+        config.generator("Ninja");
+    }
+
+    let target = config.build_target("unicorn");
     let mut dst = target.build();
     dst.push("build");
     println!("cargo:rustc-link-search=native={}", dst.display());
