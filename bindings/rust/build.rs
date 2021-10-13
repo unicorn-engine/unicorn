ebfdcf5a801f97e4000b28aea3b915b62e93e155
moonshadow565 <moonshadow565@hotmail.com>
2021-10-13 21:32:39 +0200

    Fix rust bindings not building

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,20 +1,12 @@
-use std::{env, process::Command};
+use cmake;
 
-use build_helper::rustc::{link_lib, link_search};
-
+// To get ninja fetch from here and drop .exe in your PATH:
+// https://github.com/ninja-build/ninja/releases
 fn main() {
-    println!("cargo:rerun-if-changed=unicorn");
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let unicorn = "libunicorn.a";
-    let _ = Command::new("cp")
-        .current_dir("../..")
-        .arg(&unicorn)
-        .arg(&out_dir)
-        .status()
-        .unwrap();
-    link_search(
-        Some(build_helper::SearchKind::Native),
-        build_helper::out_dir(),
-    );
-    link_lib(Some(build_helper::LibKind::Static), "unicorn");
+    let mut config = cmake::Config::new("../../.");
+    let target = config.generator("Ninja").build_target("unicorn");
+    let mut dst = target.build();
+    dst.push("build");
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=unicorn");
 }
