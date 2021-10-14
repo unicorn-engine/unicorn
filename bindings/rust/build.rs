36227f2828f52ec7b07cd54f6dcbf9e4cf531f51
Toru Ogawa <Hakuyume@users.noreply.github.com>
2021-10-14 22:46:07 +0900

    use feature flags

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,27 +1,23 @@
-use std::env;
-use std::path::PathBuf;
-
+#[cfg(feature = "build")]
 fn main() {
-    println!("cargo:rerun-if-env-changed=LIBUNICORN");
-    if let Ok(libunicorn) = env::var("LIBUNICORN") {
-        let libunicorn = PathBuf::from(libunicorn);
-        println!(
-            "cargo:rustc-link-search=native={}",
-            libunicorn.parent().unwrap().display()
-        );
-        println!("cargo:rustc-link-lib=dylib=unicorn");
-    } else {
-        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let unicorn_dir = manifest_dir.parent().unwrap().parent().unwrap();
-        println!("cargo:rerun-if-changed={}", unicorn_dir.display());
+    use std::env;
+    use std::path::PathBuf;
 
-        let install_dir = cmake::Config::new(unicorn_dir)
-            .define("UNICORN_BUILD_SHARED", "OFF")
-            .build();
-        println!(
-            "cargo:rustc-link-search=native={}",
-            install_dir.join("lib").display()
-        );
-        println!("cargo:rustc-link-lib=static=unicorn");
-    }
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let unicorn_dir = manifest_dir.parent().unwrap().parent().unwrap();
+    println!("cargo:rerun-if-changed={}", unicorn_dir.display());
+
+    let install_dir = cmake::Config::new(unicorn_dir)
+        .define("UNICORN_BUILD_SHARED", "OFF")
+        .build();
+    println!(
+        "cargo:rustc-link-search=native={}",
+        install_dir.join("lib").display(),
+    );
+    println!("cargo:rustc-link-lib=static=unicorn");
+}
+
+#[cfg(feature = "system")]
+fn main() {
+    pkg_config::probe_library("unicorn").unwrap();
 }
