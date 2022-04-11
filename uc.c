9985974998086f7fd62bc165bf667b0fc910319d
liyansong2018 <1321993383@qq.com>
2022-04-11 15:48:11 +0800

    Fix https://github.com/unicorn-engine/unicorn/issues/1586 inner free

--- uc.c
+++ uc.c
@@ -388,7 +388,7 @@ uc_err uc_open(uc_arch arch, uc_mode mode, uc_engine **result)
         }
 
         if (uc->init_arch == NULL) {
-            *result = uc;
+            free(uc);
             return UC_ERR_ARCH;
         }
 
