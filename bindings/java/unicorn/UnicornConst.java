af1c661a12ea589ff0ad94f3edf833c0f81e07ea
Mio <mio@lazym.io>
2022-07-06 09:33:45 +0800

    Update bindings

--- bindings/java/unicorn/UnicornConst.java
+++ bindings/java/unicorn/UnicornConst.java
@@ -8,13 +8,13 @@ public interface UnicornConst {
    public static final int UC_API_MINOR = 0;
 
    public static final int UC_API_PATCH = 0;
-   public static final int UC_API_EXTRA = 7;
+   public static final int UC_API_EXTRA = 255;
    public static final int UC_VERSION_MAJOR = 2;
 
    public static final int UC_VERSION_MINOR = 0;
 
    public static final int UC_VERSION_PATCH = 0;
-   public static final int UC_VERSION_EXTRA = 7;
+   public static final int UC_VERSION_EXTRA = 255;
    public static final int UC_SECOND_SCALE = 1000000;
    public static final int UC_MILISECOND_SCALE = 1000;
    public static final int UC_ARCH_ARM = 1;
@@ -36,10 +36,10 @@ public interface UnicornConst {
    public static final int UC_MODE_THUMB = 16;
    public static final int UC_MODE_MCLASS = 32;
    public static final int UC_MODE_V8 = 64;
-   public static final int UC_MODE_ARMBE8 = 128;
    public static final int UC_MODE_ARM926 = 128;
    public static final int UC_MODE_ARM946 = 256;
    public static final int UC_MODE_ARM1176 = 512;
+   public static final int UC_MODE_ARMBE8 = 1024;
    public static final int UC_MODE_MICRO = 16;
    public static final int UC_MODE_MIPS3 = 32;
    public static final int UC_MODE_MIPS32R6 = 64;
