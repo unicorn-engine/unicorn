91d00039e66b9c0d5625df9ae60aa169d063fa57
Takacs, Philipp <philipp.takacs@iosb.fraunhofer.de>
2022-10-17 14:33:10 +0200

    i386 call internal helper on special porpese register write
    Some registers writes have side effects. i.e. write to cr3 flush the tlb,
    if the PG bit is set.

--- qemu/target/i386/unicorn.c
+++ qemu/target/i386/unicorn.c
@@ -988,10 +988,16 @@ static int reg_write(CPUX86State *env, unsigned int regid, const void *value,
         default:
             break;
         case UC_X86_REG_CR0:
+            cpu_x86_update_cr0(env, *(uint32_t *)value);
+            goto write_cr;
         case UC_X86_REG_CR1:
         case UC_X86_REG_CR2:
         case UC_X86_REG_CR3:
+            cpu_x86_update_cr3(env, *(uint32_t *)value);
+            goto write_cr;
         case UC_X86_REG_CR4:
+            cpu_x86_update_cr4(env, *(uint32_t *)value);
+write_cr:
             env->cr[regid - UC_X86_REG_CR0] = *(uint32_t *)value;
             break;
         case UC_X86_REG_DR0:
@@ -1174,10 +1180,16 @@ static int reg_write(CPUX86State *env, unsigned int regid, const void *value,
         default:
             break;
         case UC_X86_REG_CR0:
+            cpu_x86_update_cr0(env, *(uint32_t *) value);
+            goto write_cr64;
         case UC_X86_REG_CR1:
         case UC_X86_REG_CR2:
         case UC_X86_REG_CR3:
+            cpu_x86_update_cr3(env, *(uint32_t *) value);
+            goto write_cr64;
         case UC_X86_REG_CR4:
+            cpu_x86_update_cr4(env, *(uint32_t *) value);
+write_cr64:
             env->cr[regid - UC_X86_REG_CR0] = *(uint64_t *)value;
             break;
         case UC_X86_REG_DR0:
