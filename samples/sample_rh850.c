117d95e01ef66f17e850e6662523be49904387a2
Damien Cauquil <dcauquil@quarkslab.com>
2023-04-13 11:10:35 +0200

    Added memory read/write hook.

--- samples/sample_rh850.c
+++ samples/sample_rh850.c
@@ -7,7 +7,7 @@
 #include <string.h>
 
 // code to be emulated
-#define RH850_CODE "\x01\x0e\x06\x00\xc1\x11" 
+#define RH850_CODE "\x01\x0e\x06\x00\xc1\x11\x01\x1f\x00\x00"
 
 // memory address where emulation starts
 #define ADDRESS 0x10000
@@ -27,13 +27,32 @@ static void hook_code(uc_engine *uc, uint64_t address, uint32_t size,
            address, size);
 }
 
+static void hook_mem64(uc_engine *uc, uc_mem_type type, uint64_t address,
+                       int size, int64_t value, void *user_data)
+{
+    switch (type) {
+    default:
+        break;
+    case UC_MEM_READ:
+        printf(">>> Memory is being READ at 0x%" PRIx64 ", data size = %u\n",
+               address, size);
+        break;
+    case UC_MEM_WRITE:
+        printf(">>> Memory is being WRITE at 0x%" PRIx64
+               ", data size = %u, data value = 0x%" PRIx64 "\n",
+               address, size, value);
+        break;
+    }
+}
+
+
 static void test_rh850(void)
 {
     uc_engine *uc;
-    uc_hook trace1, trace2;
+    uc_hook trace1, trace2, trace3;
     uc_err err;
 
-    uint64_t r1 = 2, r2 = 3;
+    uint64_t r1 = 0x10000, r2 = 3, r3;
 
     printf("Emulate RH850 code\n");
 
@@ -61,6 +80,9 @@ static void test_rh850(void)
     // tracing all instruction
     uc_hook_add(uc, &trace2, UC_HOOK_CODE, hook_code, NULL, 1, 0);
 
+    // tracing mem read
+    uc_hook_add(uc, &trace3, UC_HOOK_MEM_READ, hook_mem64, NULL, 1, 0);
+
     // emulate machine code in infinite time (last param = 0), or when
     // finishing all the code.
     err = uc_emu_start(uc, ADDRESS, ADDRESS + sizeof(RH850_CODE) - 1, 0, 0);
@@ -74,8 +96,10 @@ static void test_rh850(void)
 
     uc_reg_read(uc, UC_RH850_REG_R1, &r1);
     uc_reg_read(uc, UC_RH850_REG_R2, &r2);
+    uc_reg_read(uc, UC_RH850_REG_R3, &r3);
 
     printf(">>> R1 = 0x%" PRIx64 "\t\t>>> R2 = 0x%" PRIx64 "\n", r1, r2);
+    printf(">>> R3 = 0x%" PRIx64 "\n", r3);
 
     uc_close(uc);
 }
