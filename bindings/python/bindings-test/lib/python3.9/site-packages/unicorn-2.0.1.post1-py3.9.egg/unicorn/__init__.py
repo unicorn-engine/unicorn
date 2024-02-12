4e4181dc246b36a474f9868bf15ba02542cc9ce8
Damien Cauquil <dcauquil@quarkslab.com>
2024-02-12 11:11:29 +0100

    Remove test bindings.

--- bindings/python/bindings-test/lib/python3.9/site-packages/unicorn-2.0.1.post1-py3.9.egg/unicorn/__init__.py
+++
@@ -1,4 +0,0 @@
-# Unicorn Python bindings, by Nguyen Anh Quynnh <aquynh@gmail.com>
-from . import arm_const, arm64_const, mips_const, sparc_const, m68k_const, x86_const
-from .unicorn_const import *
-from .unicorn import Uc, uc_version, uc_arch_supported, version_bind, debug, UcError, __version__
