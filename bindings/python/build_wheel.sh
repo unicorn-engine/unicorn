d2e44472ef8baca95cc3e9a7a2543d1a7d2066cd
Bet4 <0xbet4@gmail.com>
2024-06-11 18:23:23 +0800

    Use Python 3.7 in build_wheel.sh

--- bindings/python/build_wheel.sh
+++ bindings/python/build_wheel.sh
@@ -4,11 +4,7 @@ set -e -x
 cd bindings/python
 
 # Compile wheels
-if [ -f /opt/python/cp36-cp36m/bin/python ];then
-  /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel $@
-else
-  python3 setup.py bdist_wheel $@
-fi
+python3.7 setup.py bdist_wheel $@
 cd dist
 
 # We can't repair an aarch64 wheel on x64 hosts
