2ccdc0823775d836a87dc91f76ff9f55e6c8253e
mio <mio@lazym.io>
2024-09-26 18:47:20 +0800

    Fix musl build script

--- bindings/python/musl_wheel.sh
+++ bindings/python/musl_wheel.sh
@@ -6,4 +6,4 @@ apk add gcc make cmake pkgconfig linux-headers git musl-dev patchelf
 
 python3 -m pip install -U pip setuptools auditwheel
 
-cd bindings/python && python3 setup.py bdist_wheel && auditwheel repair dist/*.whl && mv -f wheelhouse/*.whl .
+cd bindings/python && python3 setup.py bdist_wheel && auditwheel repair dist/*.whl && mv -f wheelhouse/*.whl ./dist/
