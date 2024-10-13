9f578946d5b5997cc07de1b66781b4288f75c804
Eli <elicn@users.noreply.github.com>
2024-10-13 08:14:10 +0300

    Revamp Python regression tests suite (#2022)
    * Fix Python regression test suite (partial)
    
    * Fix Python regression test suite
    
    * Add a test for mapping at high addresses
    
    * Add ctl tests

--- tests/regress/ensure_typedef_consts_generated.py
+++ tests/regress/ensure_typedef_consts_generated.py
@@ -7,9 +7,19 @@ are included in the bindings by the script for autogenerating mappings for
 constants.
 """
 
+import regress
 import unicorn
 
-try:
-    unicorn.UC_HOOK_MEM_UNMAPPED
-except AttributeError:
-    assert(False and "Definition for UC_HOOK_MEM_UNMAPPED not generated")
+
+class TestTypedefs(regress.RegressTest):
+    def runTest(self):
+        prop = 'UC_HOOK_MEM_UNMAPPED'
+
+        try:
+            getattr(unicorn, prop)
+        except AttributeError:
+            self.fail("Definition for %s not generated" % prop)
+
+
+if __name__ == '__main__':
+    regress.main()
