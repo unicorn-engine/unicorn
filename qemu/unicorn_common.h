0c73b67156200caaf3dca8d20a08efdf7ac726cc
Takacs, Philipp <philipp.takacs@iosb.fraunhofer.de>
2025-02-26 09:28:16 +0100

    add uc_mem_read_virtual
    New api to read from a vaddr. When using the MMU it's useful to direct
    read from virtual addresses.

--- qemu/unicorn_common.h
+++ qemu/unicorn_common.h
@@ -5,6 +5,7 @@
 #include "tcg/tcg.h"
 #include "qemu-common.h"
 #include "exec/memory.h"
+#include "exec/cpu_ldst.h"
 
 // This header define common patterns/codes that will be included in all arch-sepcific
 // codes for unicorns purposes.
@@ -26,6 +27,40 @@ static inline bool cpu_physical_mem_write(AddressSpace *as, hwaddr addr,
                                             const uint8_t *buf, int len)
 {
     return cpu_physical_memory_rw(as, addr, (void *)buf, len, 1);
+}
+
+static bool cpu_virtual_mem_read(struct uc_struct *uc, vaddr addr, uint32_t prot, uint8_t *buf, int len)
+{
+    MMUAccessType access_type;
+    void *hostptr;
+    int mmu_idx = cpu_mmu_index(uc->cpu->env_ptr, false);
+
+    /*
+     * Only page aligned access is allowed,
+     * because tlb_fill() might change the mappings
+     */
+    assert((addr & TARGET_PAGE_MASK) == ((addr + len - 1) & TARGET_PAGE_MASK));
+
+    switch(prot) {
+    case UC_PROT_READ:
+        access_type = MMU_DATA_LOAD;
+        break;
+    case UC_PROT_WRITE:
+        access_type = MMU_DATA_STORE;
+        break;
+    case UC_PROT_EXEC:
+        access_type = MMU_INST_FETCH;
+        break;
+    default:
+        return false;
+    }
+
+    hostptr = tlb_vaddr_to_host(uc->cpu->env_ptr, addr, access_type, mmu_idx);
+    if (!hostptr) {
+        return false;
+    }
+    memcpy(buf, hostptr, len);
+    return true;
 }
 
 void tb_cleanup(struct uc_struct *uc);
@@ -126,6 +161,7 @@ static inline void uc_common_init(struct uc_struct* uc)
 {
     uc->write_mem = cpu_physical_mem_write;
     uc->read_mem = cpu_physical_mem_read;
+    uc->read_mem_virtual = cpu_virtual_mem_read;
     uc->tcg_exec_init = tcg_exec_init;
     uc->cpu_exec_init_all = cpu_exec_init_all;
     uc->vm_start = vm_start;
