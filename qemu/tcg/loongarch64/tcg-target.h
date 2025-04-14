fb8a4f7507a5593b53e4e527e5651e881d53f270
lazymio <mio@lazym.io>
2025-04-14 23:10:26 +0800

    Fix

--- qemu/tcg/loongarch64/tcg-target.h
+++ qemu/tcg/loongarch64/tcg-target.h
@@ -185,10 +185,11 @@ typedef enum {
 #define TCG_TARGET_HAS_mulsh_i64        1
 #define TCG_TARGET_HAS_direct_jump      0
 
-#define TCG_TARGET_HAS_qemu_ldst_i128   s->use_lsx_instructions
+// TODO: use_lsx_instructions?
+#define TCG_TARGET_HAS_qemu_ldst_i128   1
 
 #define TCG_TARGET_HAS_v64              0
-#define TCG_TARGET_HAS_v128             s->use_lsx_instructions
+#define TCG_TARGET_HAS_v128             1
 #define TCG_TARGET_HAS_v256             0
 
 #define TCG_TARGET_HAS_not_vec          1
