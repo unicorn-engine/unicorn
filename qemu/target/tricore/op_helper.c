c41965e32633051d896f02d1ff231840f64bff96
appleflyerv3 <164886846+appleflyerv3@users.noreply.github.com>
2025-09-29 17:19:34 +0800

    standardize tricore
    Signed-off-by: appleflyerv3 <164886846+appleflyerv3@users.noreply.github.com>

--- qemu/target/tricore/op_helper.c
+++ qemu/target/tricore/op_helper.c
@@ -2794,7 +2794,7 @@ uint32_t helper_psw_read(CPUTriCoreState *env)
     return psw_read(env);
 }
 
-void helper_uc_tricore_exit(CPUTriCoreState *env)
+void helper_uc_exit(CPUTriCoreState *env)
 {
     CPUState *cs = env_cpu(env);
 
