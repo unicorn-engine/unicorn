6e3f1683cd66135b17f295da3f0bd0a557565b32
jesko <rattle@nullteilerfrei.de>
2026-03-26 09:33:20 +0100

    fixes #2264

--- qemu/accel/tcg/translate-all.c
+++ qemu/accel/tcg/translate-all.c
@@ -988,7 +988,7 @@ static inline void *alloc_code_gen_buffer(struct uc_struct *uc)
     memcpy(data + 0xC, (void*)&handler, 4);
 #endif
 
-    uc->seh_handle = AddVectoredExceptionHandler(0, (PVECTORED_EXCEPTION_HANDLER)closure);
+    uc->seh_handle = AddVectoredExceptionHandler(1, (PVECTORED_EXCEPTION_HANDLER)closure);
     if (!uc->seh_handle) {
         VirtualFree(uc->seh_closure, 0, MEM_RELEASE);
         uc->seh_closure = NULL;
