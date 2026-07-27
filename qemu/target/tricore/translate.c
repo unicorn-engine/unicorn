84ca3addc42b9da27273bfa159746198403b5d97
jake <jake@darkstar.cottonwood.local>
2026-07-27 08:35:11 -0400

    Scale SRO LD.H by 2
    Same format as ST.H a couple lines below. Per the aurix manual: D[15] = sign_ext(M(A[b] + zero_ext(2 * off4), half-word));

--- qemu/target/tricore/translate.c
+++ qemu/target/tricore/translate.c
@@ -4169,7 +4169,7 @@ static void decode_sro_opc(DisasContext *ctx, int op1)
         gen_offset_ld(ctx, tcg_ctx->cpu_gpr_d[15], tcg_ctx->cpu_gpr_a[r2], address, MO_UB);
         break;
     case OPC1_16_SRO_LD_H:
-        gen_offset_ld(ctx, tcg_ctx->cpu_gpr_d[15], tcg_ctx->cpu_gpr_a[r2], address, MO_LESW);
+        gen_offset_ld(ctx, tcg_ctx->cpu_gpr_d[15], tcg_ctx->cpu_gpr_a[r2], address * 2, MO_LESW);
         break;
     case OPC1_16_SRO_LD_W:
         gen_offset_ld(ctx, tcg_ctx->cpu_gpr_d[15], tcg_ctx->cpu_gpr_a[r2], address * 4, MO_LESL);
